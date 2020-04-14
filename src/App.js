d06aebf070ba4ff0f27a3a7ce7543b5155bce1d9
thanakrit pormmeepha <p00000rice@gmail.com>
2020-04-14 20:48:50 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './Pages/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NewCase from './Pages/NewCase';
+function App() {
+  return (
+    <div id="wrapper">
+    <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/NewCase" component={NewCase} />
+        </Switch>
+      </Router>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+       <footer className="sticky-footer bg-white">
+  <div className="container my-auto">
+    <div className="copyright text-center my-auto">
+      <span>Copyright © Your Website 2019</span>
+    </div>
+  </div>
+</footer>
+
+    )
+}
