ee19a82afbfe161d05ca749cc3a1378ae4291b7e
beyondsundae <p00000rice@gmail.com>
2020-06-20 22:47:32 +0700

    Updates

---
+++ precache-manifest.ca4faafed831d9bee69e4807cff69ae4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dcc5d7077f9bd9092bbb8231f475366",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d990a04e1bcb1b44ffb",
+    "url": "/static/css/2.3765edba.chunk.css"
+  },
+  {
+    "revision": "c47ef9b42b2975b08a37",
+    "url": "/static/css/main.7e5ce31d.chunk.css"
+  },
+  {
+    "revision": "6d990a04e1bcb1b44ffb",
+    "url": "/static/js/2.2101b3a6.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.2101b3a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c47ef9b42b2975b08a37",
+    "url": "/static/js/main.d4fe47b1.chunk.js"
+  },
+  {
+    "revision": "b611e47f83cf7b2106df",
+    "url": "/static/js/runtime-main.08143d21.js"
+  },
+  {
+    "revision": "1f6d643fbc008e0df97c9f01290dbaf1",
+    "url": "/static/media/MitrRegular.1f6d643f.ttf"
+  },
+  {
+    "revision": "ed9f707b75b8b7b77a3c2503ea32b8ac",
+    "url": "/static/media/exercise.ed9f707b.svg"
+  },
+  {
+    "revision": "35f48a8a388d307cfb2239883c3ab929",
+    "url": "/static/media/female.35f48a8a.svg"
+  },
+  {
+    "revision": "c8faaac9a28899f4376af976a7fe20c8",
+    "url": "/static/media/fever.c8faaac9.svg"
+  },
+  {
+    "revision": "f2270a682a0466414d4259d1be911464",
+    "url": "/static/media/funeral.f2270a68.svg"
+  },
+  {
+    "revision": "81fa839a535b981a28d5d20baf9024d1",
+    "url": "/static/media/male.81fa839a.svg"
+  },
+  {
+    "revision": "87f44aedeffd69d30f7a6b7c3a308343",
+    "url": "/static/media/patient.87f44aed.svg"
+  },
+  {
+    "revision": "b291413a07a43de06603a52cdc83a2d5",
+    "url": "/static/media/survey.b291413a.svg"
+  }
+]);
